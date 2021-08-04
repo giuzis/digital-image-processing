6ef9cc707006363e44e10782cab5ba61ac380c5b
enrico.manfron <enrico.manfron@dextra-sw.com>
2021-08-03 21:55:57 -0300

    primeiros passos

--- CountRice/main-Enrico-Giuliana.py
+++ CountRice/main-Enrico-Giuliana.py
@@ -39,8 +39,8 @@ Valor de retorno: versão binarizada da img_in.'''
     # rapidamente, e com apenas uma linha de código!
 
 #-------------------------------------------------------------------------------
-def obj_limits(pixels , alt , lar , bloob):
-    return  pixels <= bloob['n_pixels'] and alt <= bloob['B'] - bloob['T'] and lar <= bloob['R'] - bloob['L']
+def obj_limits(pixels , alt , lar , blob):
+    return  pixels <= blob['n_pixels'] and alt <= blob['B'] - blob['T'] and lar <= blob['R'] - blob['L']
 
 def img_limits(i , j , s):
     return i >= 0 and i < s[0] and j >= 0 and j < s[1] 
@@ -58,34 +58,59 @@ def rotula (img, largura_min, altura_min, n_pixels_min):
     labels = []
     for i, j, _ in np.argwhere(img > THRESHOLD): 
         if img[i][j][0] == 1:
-            bloob = {'label': len(labels)+1,'n_pixels':0,'T':i,'L':j,'B':i,'R':j}  
-            flood(bloob,img,i,j)
-            if (obj_limits(n_pixels_min, altura_min, largura_min, bloob)):
-                labels.append(bloob)
+            blob = {'label': len(labels)+1,'n_pixels':0,'T':i,'L':j,'B':i,'R':j}  
+            flood(blob,img,i,j)
+            if (obj_limits(n_pixels_min, altura_min, largura_min, blob)):
+                labels.append(blob)
         else:
             img[i][j][0] = 1.1
     return labels
 
-    '''Rotulagem usando flood fill. Marca os objetos da imagem com os valores
-[0.1,0.2,etc].
-
-Parâmetros: img: imagem de entrada E saída.
-            largura_min: descarta componentes com largura menor que esta.
-            altura_min: descarta componentes com altura menor que esta.
-            n_pixels_min: descarta componentes com menos pixels que isso.
-
-Valor de retorno: uma lista, onde cada item é um vetor associativo (dictionary)
-com os seguintes campos:
-
-'label': rótulo do componente.
-'n_pixels': número de pixels do componente.
-'T', 'L', 'B', 'R': coordenadas do retângulo envolvente de um componente conexo,
-respectivamente: topo, esquerda, baixo e direita.'''
-
-    # TODO: escreva esta função.
-    # Use a abordagem com flood fill recursivo.
-
 #===============================================================================
+def mask (img):
+        # cv2.imshow ('original', img)
+        # img = cv2.medianBlur(img,3)
+
+        # img_out = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 51, -25)
+        # cv2.imshow ('THRESHOLD', img_out)
+
+        # img = img.astype (np.float32) / 255
+        
+        # sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=3)
+        # sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=3)
+        # img_gradient = cv2.sqrt(sobelx*sobelx + sobely*sobely)
+        # img_gradient = img_gradient / 1.414
+
+        # cv2.imshow('dx',sobelx)
+        # cv2.imshow('dy',sobely)
+        # cv2.imshow('img_gradient',img_gradient)
+        
+        # kernel = np.ones((3, 3), np.uint8)
+
+        # img_erode = cv2.erode(img_out, kernel)
+        # img_dilatate = cv2.dilate(img_out, kernel)
+
+        # img_fechamento = cv2.dilate(img_erode, kernel)
+        # img_nao_fechamento = cv2.erode( img_dilatate,kernel)
+        # cv2.imshow('borderless', (img_out - img_gradient))
+        # cv2.imshow('fechamento',img_fechamento)
+        # cv2.imshow('nao_fechamento',img_nao_fechamento)
+        
+        # img_out = img_out.reshape ((img_out.shape [0], img_out.shape [1], 1))
+        # img_out = img_out.astype (np.float32) / 255
+        # print(img_out.shape)
+        # print(img_gradient.shape)
+        # cv2.imshow('multi',img_out - img_gradient*5) 
+        
+        img_out = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 51, -25)
+        kernel = np.ones((3, 3), np.uint8)
+        img_erode = cv2.erode(img_out, kernel)
+        img_fechamento = cv2.dilate(img_erode, kernel)
+        img_fechamento = img_fechamento.reshape ((img_fechamento.shape [0], img_fechamento.shape [1], 1))
+        img_fechamento = img_fechamento.astype (np.float32) / 255
+
+        return img_fechamento
+
 
 def main ():
     for INPUT_IMAGE in INPUT_IMAGES:
@@ -95,56 +120,35 @@ def main ():
         if img is None:
             print ('Erro abrindo a imagem.\n')
             sys.exit ()
-
-        # cv2.imshow ('original', img)
-        # img = cv2.medianBlur(img,3)
-
-        img_out = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 51, -25)
-        cv2.imshow ('THRESHOLD', img_out)
-        # laplacian = cv2.Laplacian(img,cv2.CV_64F)
-        img = img.reshape ((img.shape [0], img.shape [1], 1))
-        img = img.astype (np.float32) / 255
         
-        sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=3)
-        sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=3)
-        integrate = cv2.sqrt(sobelx*sobelx + sobely*sobely)*255
 
-        cv2.imshow('dx',sobelx)
-        cv2.imshow('dy',sobely)
-        cv2.imshow('integrate',integrate)
-        cv2.imshow('borderless', (img_out - integrate))
-
-        # kernel = np.ones((3, 3), np.uint8)
-        # img_erode = cv2.erode(img_out, kernel)
-        # img_erode = cv2.erode(img_erode, kernel)
-        # img_erode = cv2.erode(img_erode, kernel)
-
-        # img = cv2.GaussianBlur(img, (0,0), 1.1)
         # É uma boa prática manter o shape com 3 valores, independente da imagem ser
         # colorida ou não. Também já convertemos para float32.
         # Mantém uma cópia colorida para desenhar a saída.
-        # img_out = cv2.cvtColor (img, cv2.COLOR_GRAY2BGR)
-        # img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_out = img.reshape ((img.shape [0], img.shape [1], 1))
+        img_out = img_out.astype (np.float32) / 255
+        img_out = cv2.cvtColor(img_out, cv2.COLOR_GRAY2BGR)
 
         
         # # Segmenta a imagem.
         # if NEGATIVO:
         #     img = 1 - img
         # img = binariza (img, THRESHOLD)
+        img = mask(img)
+
         # cv2.imshow ('01 - binarizada', img)
         # cv2.imwrite ('01 - binarizada.png', img*255)
 
-        # start_time = timeit.default_timer ()
-        # componentes = rotula (img, LARGURA_MIN, ALTURA_MIN, N_PIXELS_MIN)
-        # n_componentes = len (componentes)
-        # print ('Tempo: %f' % (timeit.default_timer () - start_time))
-        # print ('%d componentes detectados.' % n_componentes)
+        componentes = rotula (img, LARGURA_MIN, ALTURA_MIN, N_PIXELS_MIN)
+        n_componentes = len (componentes)
+        print ('%d componentes detectados.' % n_componentes)
+
 
         # # Mostra os objetos encontrados.
-        # for c in componentes:
-        #     cv2.rectangle (img_out, (c ['L'], c ['T']), (c ['R'], c ['B']), (0,0,1))
+        for c in componentes:
+            cv2.rectangle (img_out, (c ['L'], c ['T']), (c ['R'], c ['B']), (0,1,0))
 
-        # cv2.imshow ('erudida', img_erode)
+        cv2.imshow ('img_out', img_out)
         
         # cv2.imwrite (INPUT_IMAGE.replace('.','_t_e_3.'), img_erode)
 
